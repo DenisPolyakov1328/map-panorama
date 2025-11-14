fbaef4d5cf59fa242759701d0821d813cf108cd0
DenisPolyakov1328 <122994941+DenisPolyakov1328@users.noreply.github.com>
2025-11-14 14:47:59 +0300

    Декомпозировал взаимодействие с слоями в директорию layers.

--- src/layers/layerConfig.ts
+++ src/layers/layerConfig.ts
@@ -1,6 +1,6 @@
-import semaphores from '../../data/semaphores.json'
-import lines from '../../data/line.json'
-import roadCros from '../../data/road_cros.json'
+import semaphores from '../data/semaphores.json'
+import lines from '../data/line.json'
+import roadCros from '../data/road_cros.json'
 
 // Open Layers накладывает слои друг на друга, хранить в правильном порядке
 export const layersConfig = [
